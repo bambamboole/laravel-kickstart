a1eacb80a4ff49f7cfc8ecb17cc865f8685a8d8c
bambamboole <manuel.christlieb@xentral.com>
2024-06-30 19:53:50 +0200

    remove obsolete stuff and change favicon. still error in browser...

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -1,10 +1,6 @@
 <script setup lang="ts">
-import { computed, ref, resolveComponent } from 'vue';
-import ApplicationLogo from '@/Components/ApplicationLogo.vue';
-import Dropdown from '@/Components/Dropdown.vue';
-import DropdownLink from '@/Components/DropdownLink.vue';
-import NavLink from '@/Components/NavLink.vue';
-import ResponsiveNavLink from '@/Components/ResponsiveNavLink.vue';
+import { ref } from 'vue';
+
 import { Link } from '@inertiajs/vue3';
 import {
     Dialog,
@@ -21,15 +17,8 @@ import {
     XMarkIcon,
     ChevronRightIcon,
 } from '@heroicons/vue/24/outline';
-import { usePage } from '@inertiajs/vue3';
 
-const teams = [
-    { id: 1, name: 'Heroicons', href: '#', initial: 'H', current: false },
-    { id: 2, name: 'Tailwind Labs', href: '#', initial: 'T', current: false },
-    { id: 3, name: 'Workcation', href: '#', initial: 'W', current: false },
-];
 const sidebarOpen = ref(false);
-const showingNavigationDropdown = ref(false);
 </script>
 
 <template>
@@ -276,142 +265,4 @@ const showingNavigationDropdown = ref(false);
             </div>
         </main>
     </div>
-    <!--    <div>-->
-    <!--        <div class="min-h-screen bg-gray-100">-->
-    <!--            <nav class="bg-white border-b border-gray-100">-->
-    <!--                &lt;!&ndash; Primary Navigation Menu &ndash;&gt;-->
-    <!--                <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">-->
-    <!--                    <div class="flex justify-between h-16">-->
-    <!--                        <div class="flex">-->
-    <!--                            &lt;!&ndash; Logo &ndash;&gt;-->
-    <!--                            <div class="shrink-0 flex items-center">-->
-    <!--                                <Link :href="route('dashboard')">-->
-    <!--                                    <ApplicationLogo-->
-    <!--                                        class="block h-9 w-auto fill-current text-gray-800"-->
-    <!--                                    />-->
-    <!--                                </Link>-->
-    <!--                            </div>-->
-
-    <!--                            &lt;!&ndash; Navigation Links &ndash;&gt;-->
-    <!--                            <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">-->
-    <!--                                <NavLink :href="route('dashboard')" :active="route().current('dashboard')">-->
-    <!--                                    Dashboard-->
-    <!--                                </NavLink>-->
-    <!--                            </div>-->
-    <!--                        </div>-->
-
-    <!--                        <div class="hidden sm:flex sm:items-center sm:ms-6">-->
-    <!--                            &lt;!&ndash; Settings Dropdown &ndash;&gt;-->
-    <!--                                <div class="ms-3 relative">-->
-    <!--                                    <Dropdown align="right" width="48">-->
-    <!--                                        <template #trigger>-->
-    <!--                                            <span class="inline-flex rounded-md">-->
-    <!--                                                <button-->
-    <!--                                                    type="button"-->
-    <!--                                                    class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150"-->
-    <!--                                                >-->
-    <!--                                                    {{ $page.props.auth.user.name }}-->
-
-    <!--                                                    <svg-->
-    <!--                                                        class="ms-2 -me-0.5 h-4 w-4"-->
-    <!--                                                        xmlns="http://www.w3.org/2000/svg"-->
-    <!--                                                        viewBox="0 0 20 20"-->
-    <!--                                                        fill="currentColor"-->
-    <!--                                                    >-->
-    <!--                                                        <path-->
-    <!--                                                            fill-rule="evenodd"-->
-    <!--                                                            d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"-->
-    <!--                                                            clip-rule="evenodd"-->
-    <!--                                                        />-->
-    <!--                                                    </svg>-->
-    <!--                                                </button>-->
-    <!--                                            </span>-->
-    <!--                                        </template>-->
-
-    <!--                                        <template #content>-->
-    <!--                                            <DropdownLink :href="route('profile.edit')"> Profile </DropdownLink>-->
-    <!--                                            <DropdownLink :href="route('logout')" method="post" as="button">-->
-    <!--                                                Log Out-->
-    <!--                                            </DropdownLink>-->
-    <!--                                        </template>-->
-    <!--                                    </Dropdown>-->
-    <!--                                </div>-->
-    <!--                        </div>-->
-
-    <!--                        &lt;!&ndash; Hamburger &ndash;&gt;-->
-    <!--                        <div class="-me-2 flex items-center sm:hidden">-->
-    <!--                            <button-->
-    <!--                                @click="showingNavigationDropdown = !showingNavigationDropdown"-->
-    <!--                                class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out"-->
-    <!--                            >-->
-    <!--                                <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">-->
-    <!--                                    <path-->
-    <!--                                        :class="{-->
-    <!--                                            hidden: showingNavigationDropdown,-->
-    <!--                                            'inline-flex': !showingNavigationDropdown,-->
-    <!--                                        }"-->
-    <!--                                        stroke-linecap="round"-->
-    <!--                                        stroke-linejoin="round"-->
-    <!--                                        stroke-width="2"-->
-    <!--                                        d="M4 6h16M4 12h16M4 18h16"-->
-    <!--                                    />-->
-    <!--                                    <path-->
-    <!--                                        :class="{-->
-    <!--                                            hidden: !showingNavigationDropdown,-->
-    <!--                                            'inline-flex': showingNavigationDropdown,-->
-    <!--                                        }"-->
-    <!--                                        stroke-linecap="round"-->
-    <!--                                        stroke-linejoin="round"-->
-    <!--                                        stroke-width="2"-->
-    <!--                                        d="M6 18L18 6M6 6l12 12"-->
-    <!--                                    />-->
-    <!--                                </svg>-->
-    <!--                            </button>-->
-    <!--                        </div>-->
-    <!--                    </div>-->
-    <!--                </div>-->
-
-    <!--                &lt;!&ndash; Responsive Navigation Menu &ndash;&gt;-->
-    <!--                <div-->
-    <!--                    :class="{ block: showingNavigationDropdown, hidden: !showingNavigationDropdown }"-->
-    <!--                    class="sm:hidden"-->
-    <!--                >-->
-    <!--                    <div class="pt-2 pb-3 space-y-1">-->
-    <!--                        <ResponsiveNavLink :href="route('dashboard')" :active="route().current('dashboard')">-->
-    <!--                            Dashboard-->
-    <!--                        </ResponsiveNavLink>-->
-    <!--                    </div>-->
-
-    <!--                    &lt;!&ndash; Responsive Settings Options &ndash;&gt;-->
-    <!--                    <div class="pt-4 pb-1 border-t border-gray-200">-->
-    <!--                        <div class="px-4">-->
-    <!--                            <div class="font-medium text-base text-gray-800">-->
-    <!--                                {{ $page.props.auth.user.name }}-->
-    <!--                            </div>-->
-    <!--                            <div class="font-medium text-sm text-gray-500">{{ $page.props.auth.user.email }}</div>-->
-    <!--                        </div>-->
-
-    <!--                        <div class="mt-3 space-y-1">-->
-    <!--                            <ResponsiveNavLink :href="route('profile.edit')"> Profile </ResponsiveNavLink>-->
-    <!--                            <ResponsiveNavLink :href="route('logout')" method="post" as="button">-->
-    <!--                                Log Out-->
-    <!--                            </ResponsiveNavLink>-->
-    <!--                        </div>-->
-    <!--                    </div>-->
-    <!--                </div>-->
-    <!--            </nav>-->
-
-    <!--            &lt;!&ndash; Page Heading &ndash;&gt;-->
-    <!--                <header class="bg-white shadow" v-if="$slots.header">-->
-    <!--                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">-->
-    <!--                        <slot name="header" />-->
-    <!--                    </div>-->
-    <!--                </header>-->
-
-    <!--            &lt;!&ndash; Page Content &ndash;&gt;-->
-    <!--            <main>-->
-    <!--                <slot />-->
-    <!--            </main>-->
-    <!--        </div>-->
-    <!--    </div>-->
 </template>
--- resources/js/Layouts/GuestLayout.vue
+++ resources/js/Layouts/GuestLayout.vue
@@ -1,5 +1,4 @@
 <script setup lang="ts">
-import ApplicationLogo from '@/Components/ApplicationLogo.vue';
 import { Link } from '@inertiajs/vue3';
 </script>
 
@@ -9,7 +8,7 @@ import { Link } from '@inertiajs/vue3';
     >
         <div>
             <Link href="/">
-                <ApplicationLogo class="h-20 w-20 fill-current text-gray-500" />
+                todo
             </Link>
         </div>
 
