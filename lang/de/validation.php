331d1ee11e7dbb03bc1cb65e147ebc3ffef54342
bambamboole <manuel.christlieb@xentral.com>
2024-07-04 13:29:55 +0200

    add missing translation

--- lang/de/validation.php
+++ lang/de/validation.php
@@ -1,6 +1,7 @@
 <?php declare(strict_types=1);
 
 return [
+    'prohibited' => 'Das :attribute Feld ist verboten.',
     'url' => 'Das Feld :attribute muss eine gültige URL sein.',
     'ulid' => 'Das Feld :attribute muss eine gültige ULID sein.',
     'uuid' => 'Das Feld :attribute muss eine gültige UUID sein.',
---
+++ resources/js/Components/Button/Button.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+defineProps<{
+    mode: string;
+}>();
+</script>
+
+<template>
+    <button
+        :class="{ 'bg-red-600': mode === 'danger', 'bg-blue-600': mode === '' }"
+        class="inline-flex items-center rounded-md border border-transparent px-4 py-2 text-xs font-semibold uppercase tracking-widest text-white transition duration-150 ease-in-out hover:bg-red-500 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 active:bg-red-700"
+    >
+        <slot />
+    </button>
+</template>
