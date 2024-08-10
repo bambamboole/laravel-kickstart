7ce4eda004ccad2ee7b729ea99514f4a38c6b9df
bambamboole <manuel.christlieb@xentral.com>
2024-08-10 12:03:26 +0200

    Extend Permission enum

--- app/Enum/Permission.php
+++ app/Enum/Permission.php
@@ -5,4 +5,11 @@ namespace App\Enum;
 enum Permission: string
 {
     case PROJECT_DELETE = 'project.delete';
+    case PROJECT_SETTINGS_VIEW = 'project.settings.view';
+    case PROJECT_MEMBERS_VIEW = 'project.members.view';
+    case PROJECT_MEMBERS_INVITE = 'project.members.invite';
+    case PROJECT_MEMBERS_DELETE = 'project.members.delete';
+    case PROJECT_INVITATIONS_DELETE = 'project.invitations.delete';
+    case PROJECT_API_TOKENS_CREATE = 'project.api-tokens.create';
+    case PROJECT_API_TOKENS_DELETE = 'project.api-tokens.delete';
 }
