f91cc2eff98be8a28326a1a8e61b84cc5b5b85c0
bambamboole <manuel.christlieb@xentral.com>
2024-08-11 18:26:49 +0200

    make email strict

--- app/Http/Requests/Projects/InviteProjectMemberRequest.php
+++ app/Http/Requests/Projects/InviteProjectMemberRequest.php
@@ -9,7 +9,7 @@ class InviteProjectMemberRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'email' => ['required', 'email'],
+            'email' => ['required', 'email:strict'],
             'role_uuid' => ['required', 'uuid', 'exists:roles,uuid'],
         ];
     }
