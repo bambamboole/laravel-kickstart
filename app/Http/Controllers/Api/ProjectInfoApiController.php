a14e6b138cba0a805935e493945c72dce9a247a4
bambamboole <manuel.christlieb@xentral.com>
2024-08-15 19:20:33 +0200

    Install bambamboole/laravel-openapi which acts as a wrapper around zircote/swagger-php and kirschbaum/openapi-validation.

---
+++ app/Http/Controllers/Api/ProjectInfoApiController.php
@@ -0,0 +1,28 @@
+<?php declare(strict_types=1);
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Resource\ProjectResource;
+use Illuminate\Http\Request;
+use OpenApi\Attributes as OA;
+
+class ProjectInfoApiController
+{
+    #[OA\Get(path: '/api/v1/info')]
+    #[OA\Response(
+        response: '200',
+        description: 'This endpoint returns information about the project, members and pending invitations.',
+        content: new OA\JsonContent(
+            properties: [new OA\Property('data', ref: '#/components/schemas/Project')],
+            additionalProperties: false,
+        )
+    )]
+    #[OA\Response(
+        response: '403',
+        description: 'Not authorized',
+    )]
+    public function __invoke(Request $request)
+    {
+        return new ProjectResource($request->project()->load(['members.pivot.role', 'invitations.role', 'tokens']));
+    }
+}
